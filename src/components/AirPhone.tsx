346220ff721c6ddd2d01176ced039d510db014b5
JMc <jkmendyka@gmail.com>
2024-05-28 13:28:57 +0200

    working on responsivness

--- src/components/AirPhone.tsx
+++ src/components/AirPhone.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { NavbarContext } from "../NavbarContext";
 import AirPhoneProductList from "./AirPhoneAPI";
 import AirPhonePNG from "../assets/airphone.png";
@@ -7,9 +7,26 @@ import "../index.css";
 function AirPhone() {
   const { navbarHeight, availableHeight } = useContext(NavbarContext);
 
+  //Dynamiczne przypisywanie klas Bootstrapa
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 700);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div
-      className="bg-image text-light flex-row-reverse d-flex"
+      className={`bg-image text-light d-flex ${
+        isMobile ? "flex-column" : "flex-row-reverse"
+      }`}
       style={{
         backgroundImage: "linear-gradient(90deg, #7d7d7d, #060606, black)",
         height: `calc(100vh - ${navbarHeight}px - 0px)`,
